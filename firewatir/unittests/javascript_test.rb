ee2f55b65409f14e81274596a78bef6cf6f77085
Stephen Anderson and Nick Karpenske <dev+bendycode+randland@bendyworks.com>
2009-11-23 11:53:30 -0600

    Expected and but was arguments were reversed.  Fixed so that error messages read correctly.

--- firewatir/unittests/javascript_test.rb
+++ firewatir/unittests/javascript_test.rb
@@ -2,13 +2,13 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..') unless $SETUP_LOADED
 require 'unittests/setup'
 
 class TC_JavaScript_Test < Test::Unit::TestCase
-    
+
 #    include FireWatir::Dialog
-    
+
     def setup
         goto_page 'JavascriptClick.html'
     end
-    
+
     tag_method :test_alert, :fails_on_ie
     def test_alert
         #browser.button(:id, "btnAlert").click_no_wait()
@@ -17,49 +17,49 @@ class TC_JavaScript_Test < Test::Unit::TestCase
         browser.startClicker("ok", 1, '', "Press OK")
         browser.button(:id, "btnAlert").click
 
-        assert_equal(browser.text_field(:id, "testResult").value , "You pressed the Alert button!")
+        assert_equal("You pressed the Alert button!", browser.text_field(:id, "testResult").value)
         assert_equal("Press OK", browser.get_popup_text)
-        
+
         browser.startClicker("ok")
         browser.button(:id, "btnAlert").click
 
-        assert_equal(browser.text_field(:id, "testResult").value , "You pressed the Alert button!")
+        assert_equal("You pressed the Alert button!", browser.text_field(:id, "testResult").value)
         assert_equal("Press OK", browser.get_popup_text)
     end
-    
+
     tag_method :test_confirm_ok, :fails_on_ie
     def test_confirm_ok
         #browser.button(:id, "btnConfirm").click_no_wait()
-        
+
         #browser.click_jspopup_button("OK")
         browser.startClicker("ok", 1, '', "Press a button")
         browser.button(:id, "btnConfirm").click
 
-        assert_equal(browser.text_field(:id, "testResult").value , "You pressed the Confirm and OK button!")
+        assert_equal("You pressed the Confirm and OK button!", browser.text_field(:id, "testResult").value)
         assert_equal("Press a button", browser.get_popup_text)
 
         browser.startClicker("ok")
         browser.button(:id, "btnConfirm").click
 
-        assert_equal(browser.text_field(:id, "testResult").value , "You pressed the Confirm and OK button!")
+        assert_equal("You pressed the Confirm and OK button!", browser.text_field(:id, "testResult").value)
         assert_equal("Press a button", browser.get_popup_text)
     end
-    
+
     tag_method :test_confirm_cancel, :fails_on_ie
     def test_confirm_cancel
         #browser.button(:id, "btnConfirm").click_no_wait()
-        
+
         #browser.click_jspopup_button("Cancel")
         browser.startClicker("cancel", 1, '', "Press a button")
         browser.button(:id, "btnConfirm").click
 
-        assert_equal(browser.text_field(:id, "testResult").value, "You pressed the Confirm and Cancel button!")
+        assert_equal("You pressed the Confirm and Cancel button!", browser.text_field(:id, "testResult").value)
         assert_equal("Press a button", browser.get_popup_text)
 
         browser.startClicker("cancel")
         browser.button(:id, "btnConfirm").click
 
-        assert_equal(browser.text_field(:id, "testResult").value, "You pressed the Confirm and Cancel button!")
+        assert_equal("You pressed the Confirm and Cancel button!", browser.text_field(:id, "testResult").value)
         assert_equal("Press a button", browser.get_popup_text)
     end
 
@@ -69,7 +69,7 @@ class TC_JavaScript_Test < Test::Unit::TestCase
         browser.startClicker("ok")
         browser.select_list(:id , "selectbox_5").select_value(/2/)
 
-        assert_equal(browser.text_field(:id, "txtAlert").value , "You pressed OK button")
+        assert_equal("You pressed OK button", browser.text_field(:id, "txtAlert").value)
         assert_equal("Press OK", browser.get_popup_text)
     end
 end
